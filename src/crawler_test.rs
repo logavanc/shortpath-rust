b1ba66908c788d32dfc7f66362f27933b6f17b10
VanCuren, Logan <lvancuren@key.net>
2022-11-18 12:14:28 -0800

    Added unit tests.

--- src/crawler_test.rs
+++ src/crawler_test.rs
@@ -23,3 +23,18 @@ fn test_shortest_working() {
     let shortest = Crawler::new(3, '…').shortest(s, others);
     assert_eq!(shortest, "abcd…");
 }
+
+#[test]
+fn test_home() {
+    let mut c = Crawler::new(3, '…');
+    c.home = Some(PathBuf::from("/home/user"));
+    assert!(c.is_home(Path::new("/home/user")));
+    assert!(!c.is_home(Path::new("/home/users")));
+}
+
+#[test]
+fn test_no_home() {
+    let mut c = Crawler::new(3, '…');
+    c.home = None;
+    assert!(!c.is_home(Path::new("/home/user")));
+}
